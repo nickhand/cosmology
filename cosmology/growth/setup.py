73b8d4d2fc2e9cc30f6ecfee95df9e43aa1c2e61
Nick Hand <nhand@berkeley.edu>
2014-12-09 14:29:04 -0800

    changes to setup.py to include the gsl include dir in compilation

--- cosmology/growth/setup.py
+++ cosmology/growth/setup.py
@@ -9,7 +9,7 @@ base_path = os.path.abspath(os.path.dirname(__file__))
 
 def configuration(parent_package='', top_path=None):
     from numpy.distutils.misc_util import Configuration, get_numpy_include_dirs
-    from cython_gsl import get_cython_include_dir, get_libraries, get_library_dir
+    from cython_gsl import get_include, get_libraries, get_library_dir
     
     config = Configuration('growth', parent_package, top_path)
 
@@ -20,25 +20,25 @@ def configuration(parent_package='', top_path=None):
     cython(['emulator.pyx'], working_path=base_path)
 
     config.add_extension('core', sources=['core.c', 'transfer.c', 'power_tools.c'],
-                         include_dirs=[get_numpy_include_dirs(), get_cython_include_dir(), ''],
+                         include_dirs=[get_numpy_include_dirs(), get_include(), ''],
                          libraries=get_libraries(), library_dirs=[get_library_dir()],
                          extra_compile_args=['-O3', '-w'],
                          extra_link_args=['-g'])
 
     config.add_extension('power', sources=['power.c', 'transfer.c', 'power_tools.c'],
-                         include_dirs=[get_numpy_include_dirs(), get_cython_include_dir(), ''],
+                         include_dirs=[get_numpy_include_dirs(), get_include(), ''],
                          libraries=get_libraries(), library_dirs=[get_library_dir()],
                          extra_compile_args=['-O3', '-w'],
                          extra_link_args=['-g'])
     
     config.add_extension('correlation', sources=['correlation.c', 'power_tools.c', 'transfer.c'],
-                         include_dirs=[get_numpy_include_dirs(), get_cython_include_dir(), ''],
+                         include_dirs=[get_numpy_include_dirs(), get_include(), ''],
                          libraries=get_libraries(), library_dirs=[get_library_dir()],
                          extra_compile_args=['-O3', '-w'],
                          extra_link_args=['-g'])
 
     config.add_extension('lensing', sources=['lensing.c', 'power_tools.c', 'transfer.c'],
-                         include_dirs=[get_numpy_include_dirs(), get_cython_include_dir(), ''],
+                         include_dirs=[get_numpy_include_dirs(), get_include(), ''],
                          libraries=get_libraries(), library_dirs=[get_library_dir()],
                          extra_compile_args=['-O3', '-w'],
                          extra_link_args=['-g'])
@@ -47,7 +47,7 @@ def configuration(parent_package='', top_path=None):
     sources = ['emulator.c', 'emulator/core.c', 'emulator/emu.c', 'emulator/emu_noh.c', \
                 'emulator/hubble.c', 'hod_emulator/core.c', 'hod_emulator/emu.c']
     config.add_extension('emulator', sources=sources ,
-                          include_dirs=[get_numpy_include_dirs(), get_cython_include_dir(), '', 'emulator', 'hod_emulator'],
+                          include_dirs=[get_numpy_include_dirs(), get_include(), '', 'emulator', 'hod_emulator'],
                           libraries=get_libraries(), library_dirs=[get_library_dir(), 'emulator', 'hod_emulator'],
                           extra_compile_args=['-O3', '-w'],
                           extra_link_args=['-g'])
--- cosmology/halo/setup.py
+++ cosmology/halo/setup.py
@@ -7,14 +7,14 @@ base_path = os.path.abspath(os.path.dirname(__file__))
 
 def configuration(parent_package='', top_path=None):
     from numpy.distutils.misc_util import Configuration, get_numpy_include_dirs
-    from cython_gsl import get_cython_include_dir, get_libraries, get_library_dir
+    from cython_gsl import get_include, get_libraries, get_library_dir
     
     config = Configuration('halo', parent_package, top_path)
 
     cython(['bias.pyx'], working_path=base_path)
     
     config.add_extension('bias', sources=['bias.c', 'halo_tools.c'],
-                         include_dirs=[get_numpy_include_dirs(), get_cython_include_dir(), ''],
+                         include_dirs=[get_numpy_include_dirs(), get_include(), ''],
                          libraries=get_libraries(), library_dirs=[get_library_dir()],
                          extra_compile_args=['-O3', '-w'],
                          extra_link_args=['-g'])
